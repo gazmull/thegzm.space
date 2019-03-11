a28bf79fe3fdf2e5513bb4189d58e8ea2c72a80e
Euni <vescalaw@gmail.com>
2019-03-12 04:38:12 +0800

    Build Docs

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(() => {
+    $('.project')
+        .on('mouseenter', ({ currentTarget: $this }) => {
+        const name = $($this).attr('name');
+        if (!name)
+            return;
+        $(`#img-${name.replace(/[^\w]/g, '')}`).removeClass('invisible');
+    })
+        .on('mouseleave', ({ currentTarget: $this }) => {
+        const name = $($this).attr('name');
+        if (!name)
+            return;
+        $(`#img-${name.replace(/[^\w]/g, '')}`).addClass('invisible');
+    });
+});
